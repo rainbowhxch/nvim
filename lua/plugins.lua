0592fa6c92bca16edc66ef02b7112f9446bdc048
rainbowhxch <rainbowhxch@gmail.com>
2021-03-30 15:09:16 +0800

    lua: all done

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,72 +8,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
-
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  -- decoration
-  use 'windwp/nvim-autopairs'
-  use 'Yggdroot/indentLine'
-  use {'lukas-reineke/indent-blankline.nvim', branch = 'lua'}
-  use 'kyazdani42/nvim-web-devicons'
-  use 'glepnir/dashboard-nvim'
-  use 'glepnir/galaxyline.nvim'
-  use 'romgrk/barbar.nvim'
-  use 'aklt/plantuml-syntax'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'p00f/nvim-ts-rainbow', requires = {'nvim-treesitter/nvim-treesitter'} }
-  use { 'windwp/nvim-ts-autotag', requires = {'nvim-treesitter/nvim-treesitter'} }
-  use { 'JoosepAlviste/nvim-ts-context-commentstring', requires = {'nvim-treesitter/nvim-treesitter'} }
-
-  -- reading mode
-  use { 'junegunn/goyo.vim', cmd = 'Goyo' }
-  use { 'junegunn/limelight.vim', cmd = 'Goyo' }
-
-  -- functional
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
-  use { 'nvim-telescope/telescope-media-files.nvim', requires = {'nvim-telescope/telescope.nvim'} }
-  use { 'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'} }
-  use 'honza/vim-snippets'
-  use {'iamcco/markdown-preview.nvim', ft = {'markdown'}, run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-  use { 'mzlogin/vim-markdown-toc', ft = {'markdown'} }
-  use { 'dhruvasagar/vim-table-mode', ft = {'markdown'} }
-  use { 'lervag/vimtex', ft = {'tex'} }
-  use 'junegunn/vim-easy-align'
-  use 'liuchengxu/vista.vim'
-  use 'kshenoy/vim-signature'
-  use 'phaazon/hop.nvim'
-  use 'MattesGroeger/vim-bookmarks'
-  use 'lambdalisue/suda.vim'
-  use 'skywind3000/asynctasks.vim'
-  use 'skywind3000/asyncrun.vim'
-  use 'voldikss/vim-floaterm'
-  use 'tpope/vim-surround'
-  use 'AndrewRadev/splitjoin.vim'
-  use 'andymass/vim-matchup'
-  use { 'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
-  use 'terrortylor/nvim-comment'
-  use 'tpope/vim-repeat'
-  use 'brooth/far.vim'
-  use { 'liuchengxu/vim-which/key', cmd = {'WhichKey', 'WhichKey!'} }
-  use { 'pechorin/any-jump.vim', cmd = {'AnyJump'} }
-  use { 'drmikehenry/vim-headerguard', ft = {'c', 'cpp'}, cmd = {'HeaderguardAdd'} }
-  use { 'mbbill/undotree', cmd = {'UndotreeToggle'} }
-  use 'unblevable/quick-scope'
-  use 'Shougo/echodoc.vim'
-  use 'rhysd/accelerated-jk'
-  use 'wellle/context.vim'
-  use 'kevinhwang91/nvim-hlslens'
-  use 'junegunn/vim-peekaboo'
-  use 'mhartington/formatter.nvim'
-  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
-  use 'monaqa/dial.nvim'
-
-  -- themes
-  use 'morhetz/gruvbox'
-end)
-
 vim.g.gruvbox_italic = 1
 vim.g.gruvbox_italicize_strings = 1
 vim.g.gruvbox_invert_signs = 1
@@ -118,7 +52,7 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
---- formatter.nvim
+-- formatter.nvim
 require('formatter').setup({
   logging = false,
   filetype = {
@@ -219,9 +153,9 @@ utils.nmap('k', '<Plug>(accelerated_jk_gk)')
 vim.g.echodoc_enable_at_startup = 1
 
 -- undotree.vim
-utils.nnoremap('<LEADER>U', '<CMD>UndotreeToggle<CR>')
+utils.nnoremap('U', '<CMD>UndotreeToggle<CR>')
 vim.cmd([[
-    function g:Undotree_CustomMap()
+    function! g:Undotree_CustomMap()
         nmap <buffer> J <plug>UndotreeNextState
         nmap <buffer> K <plug>UndotreePreviousState
     endfunc
@@ -453,72 +387,72 @@ vim.g.indent_blankline_context_patterns = {
 local actions = require('telescope.actions')
 require('telescope').load_extension('media_files')
 require('telescope').setup {
-    defaults = {
-        vimgrep_arguments = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
-        prompt_position = "top",
-        prompt_prefix = " ",
-        selection_caret = " ",
-        entry_prefix = "  ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "descending",
-        layout_strategy = "horizontal",
-        layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
-        file_sorter = require'telescope.sorters'.get_fuzzy_file,
-        file_ignore_patterns = {},
-        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
-        shorten_path = true,
-        winblend = 0,
-        width = 0.75,
-        preview_cutoff = 120,
-        results_height = 1,
-        results_width = 0.8,
-        border = {},
-        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-        color_devicons = true,
-        use_less = true,
-        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
-        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+ defaults = {
+    vimgrep_arguments = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+    prompt_position = "top",
+    prompt_prefix = " ",
+    selection_caret = " ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
+    file_sorter = require'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+    shorten_path = true,
+    winblend = 0,
+    width = 0.75,
+    preview_cutoff = 120,
+    results_height = 1,
+    results_width = 0.8,
+    border = {},
+    borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+    color_devicons = true,
+    use_less = true,
+    set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
 
-        -- Developer configurations: Not meant for general override
-        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-                -- To disable a keymap, put [map] = false
-                -- So, to not map "<C-n>", just put
-                -- ["<c-x>"] = false,
-                ["<esc>"] = actions.close,
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        -- To disable a keymap, put [map] = false
+        -- So, to not map "<C-n>", just put
+        -- ["<c-x>"] = false,
+        ["<esc>"] = actions.close,
 
-                -- Otherwise, just set the mapping to the function that you want it to be.
-                -- ["<C-i>"] = actions.select_horizontal,
+        -- Otherwise, just set the mapping to the function that you want it to be.
+        -- ["<C-i>"] = actions.select_horizontal,
 
-                -- Add up multiple actions
-                ["<CR>"] = actions.select_default + actions.center
+        -- Add up multiple actions
+        ["<CR>"] = actions.select_default + actions.center
 
-                -- You can perform as many actions in a row as you like
-                -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
-            },
-            n = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-                -- ["<C-i>"] = my_cool_custom_action,
-            }
-        }
-    },
-    extensions = {
-        media_files = {
-            -- filetypes whitelist
-            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-            filetypes = {"png", "webp", "jpg", "jpeg"},
-            find_cmd = "rg" -- find command (defaults to `fd`)
-        }
+        -- You can perform as many actions in a row as you like
+        -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
+      },
+      n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        -- ["<C-i>"] = my_cool_custom_action,
+      }
     }
+  },
+  extensions = {
+    media_files = {
+      -- filetypes whitelist
+      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+      filetypes = {"png", "webp", "jpg", "jpeg"},
+      find_cmd = "rg" -- find command (defaults to `fd`)
+    }
+  }
 }
 
 -- nvim-tree
@@ -533,20 +467,20 @@ vim.g.nvim_tree_bindings = {
   ["o"]              = tree_cb("edit"),
   ["<2-LeftMouse>"]  = tree_cb("edit"),
   ["<2-RightMouse>"] = tree_cb("cd"),
-  ["<l>"]          = tree_cb("cd"),
-  ["<v>"]          = tree_cb("vsplit"),
-  ["<s>"]          = tree_cb("split"),
-  ["<t>"]          = tree_cb("tabnew"),
+  ["<l>"]            = tree_cb("cd"),
+  ["<v>"]            = tree_cb("vsplit"),
+  ["<s>"]            = tree_cb("split"),
+  ["<t>"]            = tree_cb("tabnew"),
   ["<BS>"]           = tree_cb("close_node"),
   ["<S-CR>"]         = tree_cb("close_node"),
   ["<Tab>"]          = tree_cb("preview"),
   ["I"]              = tree_cb("toggle_ignored"),
   ["H"]              = tree_cb("toggle_dotfiles"),
-  ["<C-r>"]              = tree_cb("refresh"),
+  ["<C-r>"]          = tree_cb("refresh"),
   ["a"]              = tree_cb("create"),
   ["d"]              = tree_cb("remove"),
   ["r"]              = tree_cb("rename"),
-  ["R"]          = tree_cb("full_rename"),
+  ["R"]              = tree_cb("full_rename"),
   ["c"]              = tree_cb("cut"),
   ["y"]              = tree_cb("copy"),
   ["p"]              = tree_cb("paste"),
@@ -556,22 +490,22 @@ vim.g.nvim_tree_bindings = {
   ["q"]              = tree_cb("close"),
 }
 vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    git = {
-        unstaged = "",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = ""
-    },
-    folder = {
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = ""
-    }
+  default = '',
+  symlink = '',
+  git = {
+    unstaged = "",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    untracked = ""
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = ""
+  }
 }
 
 -- dashboard-nvim
@@ -608,3 +542,69 @@ utils.nmap('<C-a>', '<Plug>(dial-increment)')
 utils.nmap('<C-x>', '<Plug>(dial-decrement)')
 utils.vmap('<C-a>', '<Plug>(dial-increment)')
 utils.vmap('<C-x>', '<Plug>(dial-decrement)')
+
+return require('packer').startup(function(use)
+  -- packer.nvim itself
+  use 'wbthomason/packer.nvim'
+
+  -- decoration
+  use 'windwp/nvim-autopairs'
+  use { 'lukas-reineke/indent-blankline.nvim', branch = 'lua' }
+  use 'kyazdani42/nvim-web-devicons'
+  use 'glepnir/dashboard-nvim'
+  use 'glepnir/galaxyline.nvim'
+  use 'romgrk/barbar.nvim'
+  use 'aklt/plantuml-syntax'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'p00f/nvim-ts-rainbow', requires = {'nvim-treesitter/nvim-treesitter'} }
+  use { 'windwp/nvim-ts-autotag', requires = {'nvim-treesitter/nvim-treesitter'} }
+  use { 'JoosepAlviste/nvim-ts-context-commentstring', requires = {'nvim-treesitter/nvim-treesitter'} }
+
+  -- reading mode
+  use { 'junegunn/goyo.vim', cmd = 'Goyo' }
+  use { 'junegunn/limelight.vim', cmd = 'Goyo' }
+
+  -- functional
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
+  use { 'nvim-telescope/telescope-media-files.nvim', requires = {'nvim-telescope/telescope.nvim'} }
+  use { 'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'} }
+  use 'honza/vim-snippets'
+  use {'iamcco/markdown-preview.nvim', ft = {'markdown'}, run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+  use { 'mzlogin/vim-markdown-toc', ft = {'markdown'} }
+  use { 'dhruvasagar/vim-table-mode', ft = {'markdown'} }
+  use { 'lervag/vimtex', ft = {'tex'} }
+  use 'junegunn/vim-easy-align'
+  use 'liuchengxu/vista.vim'
+  use 'kshenoy/vim-signature'
+  use 'phaazon/hop.nvim'
+  use 'MattesGroeger/vim-bookmarks'
+  use 'lambdalisue/suda.vim'
+  use 'skywind3000/asynctasks.vim'
+  use 'skywind3000/asyncrun.vim'
+  use 'voldikss/vim-floaterm'
+  use 'tpope/vim-surround'
+  use 'AndrewRadev/splitjoin.vim'
+  use 'andymass/vim-matchup'
+  use { 'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
+  use 'terrortylor/nvim-comment'
+  use 'tpope/vim-repeat'
+  use 'brooth/far.vim'
+  use 'liuchengxu/vim-which-key'
+  use { 'pechorin/any-jump.vim', cmd = {'AnyJump'} }
+  use { 'drmikehenry/vim-headerguard', ft = {'c', 'cpp'}, cmd = {'HeaderguardAdd'} }
+  use { 'mbbill/undotree', cmd = {'UndotreeToggle'} }
+  use 'unblevable/quick-scope'
+  use 'Shougo/echodoc.vim'
+  use 'rhysd/accelerated-jk'
+  use 'wellle/context.vim'
+  use 'kevinhwang91/nvim-hlslens'
+  use 'junegunn/vim-peekaboo'
+  use 'mhartington/formatter.nvim'
+  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+  use 'monaqa/dial.nvim'
+
+  -- themes
+  use 'morhetz/gruvbox'
+end)
---
+++ plugin/packer_compiled.vim
@@ -0,0 +1,295 @@
+" Automatically generated packer.nvim plugin loader code
+
+if !has('nvim-0.5')
+  echohl WarningMsg
+  echom "Invalid Neovim version for packer.nvim!"
+  echohl None
+  finish
+endif
+
+packadd packer.nvim
+
+try
+
+lua << END
+local package_path_str = "/home/ch/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/ch/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/ch/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/ch/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/ch/.cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s))
+  if not success then
+    print('Error running ' .. component .. ' for ' .. name)
+    error(result)
+  end
+  return result
+end
+
+_G.packer_plugins = {
+  ["accelerated-jk"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/accelerated-jk"
+  },
+  ["any-jump.vim"] = {
+    commands = { "AnyJump" },
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/any-jump.vim"
+  },
+  ["asyncrun.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/asyncrun.vim"
+  },
+  ["asynctasks.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/asynctasks.vim"
+  },
+  ["barbar.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/barbar.nvim"
+  },
+  ["context.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/context.vim"
+  },
+  ["dashboard-nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/dashboard-nvim"
+  },
+  ["dial.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/dial.nvim"
+  },
+  ["echodoc.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/echodoc.vim"
+  },
+  ["far.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/far.vim"
+  },
+  firenvim = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/firenvim"
+  },
+  ["formatter.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/formatter.nvim"
+  },
+  ["galaxyline.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/galaxyline.nvim"
+  },
+  ["gitsigns.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
+  },
+  ["goyo.vim"] = {
+    commands = { "Goyo" },
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/goyo.vim"
+  },
+  gruvbox = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/gruvbox"
+  },
+  ["hop.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/hop.nvim"
+  },
+  ["indent-blankline.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim"
+  },
+  ["limelight.vim"] = {
+    commands = { "Goyo" },
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/limelight.vim"
+  },
+  ["markdown-preview.nvim"] = {
+    commands = { "MarkdownPreview" },
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/markdown-preview.nvim"
+  },
+  ["nvim-autopairs"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-autopairs"
+  },
+  ["nvim-comment"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-comment"
+  },
+  ["nvim-hlslens"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-hlslens"
+  },
+  ["nvim-tree.lua"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+  },
+  ["nvim-ts-autotag"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag"
+  },
+  ["nvim-ts-context-commentstring"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring"
+  },
+  ["nvim-ts-rainbow"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/packer.nvim"
+  },
+  ["plantuml-syntax"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/plantuml-syntax"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+  },
+  ["popup.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/popup.nvim"
+  },
+  ["quick-scope"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/quick-scope"
+  },
+  ["splitjoin.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/splitjoin.vim"
+  },
+  ["suda.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/suda.vim"
+  },
+  ["telescope-media-files.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/telescope-media-files.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/telescope.nvim"
+  },
+  undotree = {
+    commands = { "UndotreeToggle" },
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/undotree"
+  },
+  ["vim-bookmarks"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-bookmarks"
+  },
+  ["vim-easy-align"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-easy-align"
+  },
+  ["vim-floaterm"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-floaterm"
+  },
+  ["vim-headerguard"] = {
+    commands = { "HeaderguardAdd" },
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/vim-headerguard"
+  },
+  ["vim-markdown-toc"] = {
+    loaded = false,
+    needs_bufread = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/vim-markdown-toc"
+  },
+  ["vim-matchup"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-matchup"
+  },
+  ["vim-peekaboo"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-peekaboo"
+  },
+  ["vim-repeat"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-repeat"
+  },
+  ["vim-signature"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-signature"
+  },
+  ["vim-snippets"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-snippets"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-surround"
+  },
+  ["vim-table-mode"] = {
+    loaded = false,
+    needs_bufread = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/vim-table-mode"
+  },
+  ["vim-which-key"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vim-which-key"
+  },
+  vimtex = {
+    loaded = false,
+    needs_bufread = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/opt/vimtex"
+  },
+  ["vista.vim"] = {
+    loaded = true,
+    path = "/home/ch/.local/share/nvim/site/pack/packer/start/vista.vim"
+  }
+}
+
+
+-- Command lazy-loads
+vim.cmd [[command! -nargs=* -range -bang -complete=file MarkdownPreview lua require("packer.load")({'markdown-preview.nvim'}, { cmd = "MarkdownPreview", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args> }, _G.packer_plugins)]]
+vim.cmd [[command! -nargs=* -range -bang -complete=file Goyo lua require("packer.load")({'goyo.vim', 'limelight.vim'}, { cmd = "Goyo", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args> }, _G.packer_plugins)]]
+vim.cmd [[command! -nargs=* -range -bang -complete=file HeaderguardAdd lua require("packer.load")({'vim-headerguard'}, { cmd = "HeaderguardAdd", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args> }, _G.packer_plugins)]]
+vim.cmd [[command! -nargs=* -range -bang -complete=file UndotreeToggle lua require("packer.load")({'undotree'}, { cmd = "UndotreeToggle", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args> }, _G.packer_plugins)]]
+vim.cmd [[command! -nargs=* -range -bang -complete=file AnyJump lua require("packer.load")({'any-jump.vim'}, { cmd = "AnyJump", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args> }, _G.packer_plugins)]]
+
+vim.cmd [[augroup packer_load_aucmds]]
+vim.cmd [[au!]]
+  -- Filetype lazy-loads
+vim.cmd [[au FileType c ++once lua require("packer.load")({'vim-headerguard'}, { ft = "c" }, _G.packer_plugins)]]
+vim.cmd [[au FileType cpp ++once lua require("packer.load")({'vim-headerguard'}, { ft = "cpp" }, _G.packer_plugins)]]
+vim.cmd [[au FileType markdown ++once lua require("packer.load")({'vim-markdown-toc', 'vim-table-mode', 'markdown-preview.nvim'}, { ft = "markdown" }, _G.packer_plugins)]]
+vim.cmd [[au FileType tex ++once lua require("packer.load")({'vimtex'}, { ft = "tex" }, _G.packer_plugins)]]
+vim.cmd("augroup END")
+vim.cmd [[augroup filetypedetect]]
+vim.cmd [[source /home/ch/.local/share/nvim/site/pack/packer/opt/vim-markdown-toc/ftdetect/markdown.vim]]
+vim.cmd [[source /home/ch/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tex.vim]]
+vim.cmd("augroup END")
+END
+
+catch
+  echohl ErrorMsg
+  echom "Error in packer_compiled: " .. v:exception
+  echom "Please check your config for correctness"
+  echohl None
+endtry
